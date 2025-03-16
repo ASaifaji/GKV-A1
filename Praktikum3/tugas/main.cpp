4b46059befc764a4e94aa0a41da7bc655d8a14a8
ASaifaji <aby110605@gmail.com>
2025-03-16 15:51:34 +0700

    Tugas Praktikum 3 Modul 4
    16/03/2025

--- Praktikum3/tugas/main.cpp
+++ Praktikum3/tugas/main.cpp
@@ -1,6 +1,8 @@
 #include <GL/glut.h>
 
-static int shoulder = 0, elbow = 0, wrist = 0;
+static int shoulder = 0, elbow = 0, wrist = 0, pinky = 0, ring = 0, middle = 0, index = 0, thumb = 0, thumb2 = 0, thumb_angle = 0;
+static float thumb_rad = 0.0;
+
 
 void init(void) {
     glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -12,29 +14,79 @@ void display(void) {
     glPushMatrix();
 
     glTranslatef(-3.0, 0.0, 0.0);
-    glRotatef((GLfloat)shoulder, 0.0, 0.0, 1.0);
+    glRotatef((GLfloat)shoulder, 0.0, 1.0, 0.0);
     glTranslatef(1.0, 0.0, 0.0);
     glPushMatrix();
     
-    glScalef(2.0, 0.4, 1.0);
+    glScalef(2.0, 0.6, 0.4);
     glutWireCube(1.0);
     glPopMatrix();
 
     glTranslatef(1.0, 0.0, 0.0);
-    glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
+    glRotatef((GLfloat)elbow, 0.0, 1.0, 0.0);
     glTranslatef(1.0, 0.0, 0.0);
     glPushMatrix();
     
-    glScalef(2.0, 0.4, 1.0);
+    glScalef(2.0, 0.6, 0.4);
     glutWireCube(1.0);
     glPopMatrix();
     
     glTranslatef(1.0,0.0,0.0);
-    glRotatef((GLfloat)wrist, 0.0, 0.0, 1.0);
-    glTranslatef(1.0,0.0,0.0);
+    glRotatef((GLfloat)wrist, 0.0, 1.0, 0.0);
+    glTranslatef(0.4,0.0,0.0);
     glPushMatrix();
     
-    glScalef(2.0,0.4,1.0);
+    glScalef(0.8, 1.1, 0.4);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    glTranslatef(0.4,-0.45,0.0);
+    glRotatef(pinky, 0.0, 1.0, 0.0);
+    glTranslatef(0.3,0.0,0.0);
+    glPushMatrix();
+    
+    glScalef(0.6, 0.2, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    glTranslatef(-0.3,0.3,0.0);
+    glRotatef((GLfloat)ring, 0.0, 1.0, 0.0);
+    glTranslatef(0.3,0.0,0.0);
+    glPushMatrix();
+    
+    glScalef(0.6, 0.2, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    glTranslatef(-0.3,0.3,0.0);
+    glRotatef((GLfloat)middle, 0.0, 1.0, 0.0);
+    glTranslatef(0.3,0.0,0.0);
+    glPushMatrix();
+    
+    glScalef(0.6, 0.2, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    glTranslatef(-0.3,0.3,0.0);
+    glRotatef((GLfloat)index, 0.0, 1.0, 0.0);
+    glTranslatef(0.3,0.0,0.0);
+    glPushMatrix();
+    
+    glScalef(0.6, 0.2, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+    
+    // 
+    glTranslatef(-0.3,0.0,0.0);
+    glRotatef((GLfloat)thumb_angle, 0.0,1.0,0.0);
+    glTranslatef(-0.4,0.1,0.0);
+    glRotatef(45, 0.0, 0.0, 1.0);
+    glRotatef((GLfloat)thumb, 0.0, 1.0, 0.0);
+    glRotatef((GLfloat)thumb2, 0.0, 0.0, 1.0);
+    glTranslatef(0.3,0.0,0.0);
+    glPushMatrix();
+    
+    glScalef(0.5, 0.2, 0.2);
     glutWireCube(1.0);
     glPopMatrix();
     
@@ -54,23 +106,123 @@ void reshape(int w, int h) {
 
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
-        case 119:
+        case 119: // w
+        	if(shoulder >= 55){
+        		break;
+        	}
             shoulder = (shoulder + 5) % 360;
             break;
-        case 115:
+        case 115: // s
+        	if(shoulder <= -55){
+        		break;
+        	}
             shoulder = (shoulder - 5) % 360;
             break;
-        case 'e':
+        case 101: // e
+        	if(elbow >= 0){
+        		break;
+        	}
             elbow = (elbow + 5) % 360;
             break;
-        case 'd':
+        case 100: // d
+        	if(elbow <= -140){
+        		break;
+        	}
             elbow = (elbow - 5) % 360;
             break;
-        case 'r':
+        case 114: // r
+        	if(wrist >= 90){
+        		break;
+        	}
         	wrist = (wrist + 5) % 360;
         	break;
-        case 'f':
+        case 102: // f
+        	if(wrist <= -90){
+        		break;
+        	}
         	wrist = (wrist - 5) % 360;
+        	break;
+        case 116: // t
+        	if(thumb >= 5){
+        		break;
+        	}
+        	else{
+        		if(thumb < -90){
+        			thumb = (thumb + 5) % 360;
+        			thumb2 = (thumb2 + 5) % 360;
+        			break;
+        		}
+        		thumb = (thumb + 5) % 360;
+        		break;
+        	}
+        case 103: // g
+        	if(thumb <= -90){
+        		if(thumb <= -150){
+        			break;
+        		}
+        		thumb = (thumb - 5 ) % 360;
+        		thumb2 = (thumb2 - 5 ) % 360;
+        		break;
+        	}
+        	else{
+        		thumb = (thumb - 5) % 360;
+	        	break;
+        	}
+        case 121: // y
+        	if(index >= 10){
+        		break;
+        	}
+        	index = (index + 5) % 360;
+        	thumb_angle = (thumb_angle - 5) % 360;
+         	break;
+        case 104: // h
+        	if(index <= -150){
+        		break;
+        	}
+        	index = (index - 5) % 360;
+        	thumb_angle = (thumb_angle + 5) % 360;
+        	break;
+        case 117: // u
+        	if(middle >= 10){
+        		break;
+        	}
+        	middle = (middle + 5) % 360;
+        	index = (index - 5) % 360;
+        	break;
+        case 106: // j
+        	if(middle <= -150){
+        		break;
+        	}
+        	middle = (middle - 5) % 360;
+        	index = (index + 5) % 360;
+        	break;
+        case 105: // i
+        	if(ring >= 10){
+        		break;
+        	}
+        	ring = (ring + 5) % 360;
+        	middle = (middle - 5) % 360;
+        	break;
+        case 107: // k
+        	if(ring <= -150){
+        		break;
+        	}
+        	ring = (ring - 5) % 360;
+        	middle = (middle + 5) % 360;
+        	break;
+        case 111: // o
+        	if(pinky >= 10){
+        		break;
+        	}
+        	pinky = (pinky + 5) % 360;
+        	ring = (ring - 5) % 360;
+        	break;
+        case 108: // l
+        	if(pinky <= -150){
+        		break;
+        	}
+        	pinky = (pinky - 5) % 360;
+        	ring = (ring + 5) % 360;
         	break;
         case 27: // Escape key
             exit(0);
@@ -84,7 +236,7 @@ void keyboard(unsigned char key, int x, int y) {
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(700, 600);
+    glutInitWindowSize(1360, 720);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Robot Arm");
     init();
